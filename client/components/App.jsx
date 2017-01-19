dce5adb59abe506084f04b361db7d47179f5f767
bartonyoung <barton.young1986@gmail.com>
2017-01-19 13:38:23 -0800

    basic top levl component

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
