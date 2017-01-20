eb5c509153d405be955b4843e5cb66d38c1ec043
MarcManiez <marc.maniez@gmail.com[A>
2017-01-19 19:41:44 -0800

    Realize this may have been done for nothing.

--- database/sql-connection.js
+++ database/sql-connection.js
@@ -11,8 +11,8 @@ module.exports = connection;
 
 const post = {
   comment: 'FIRST!!!',
-  user_id: 1,
-  video_id: 1,
+  // user_id: 1,
+  // video_id: 1,
   time_stamp: 11.111
 };
 
@@ -22,7 +22,10 @@ connection.query('INSERT INTO Users SET ?', {name: 'MARC'}, (err, rows, fields) 
   }
 });
 
-connection.query('INSERT INTO Videos SET ?', {url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', user_id: 1}, (err, rows, fields) => {
+connection.query('INSERT INTO Videos SET ?', {
+  url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  // , user_id: 1
+}, (err, rows, fields) => {
   if (err) {
     console.log(err);
   }
