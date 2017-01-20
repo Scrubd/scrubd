9eec5df7693b796a9f8367ba1e8f2ddb7dc1b358
MarcManiez <marc.maniez@gmail.com[A>
2017-01-19 16:06:38 -0800

    Add some test querries.

--- database/sql-connection.js
+++ database/sql-connection.js
@@ -9,3 +9,27 @@ const connection =  mysql.createConnection({
 
 module.exports = connection;
 
+const post = {
+  comment: 'FIRST!!!',
+  user_id: 1,
+  video_id: 1,
+  time_stamp: 11.111
+};
+
+connection.query('INSERT INTO Users SET ?', {name: 'MARC'}, (err, rows, fields) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+connection.query('INSERT INTO Videos SET ?', {url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', user_id: 1}, (err, rows, fields) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+connection.query('INSERT INTO Comments SET ?', post, (err, rows, fields) => {
+  if (err) {
+    console.log(err);
+  }
+});
