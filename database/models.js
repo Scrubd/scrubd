371359d279ef7168671c3e0ddcee3c1ebdf21426
MarcManiez <marc.maniez@gmail.com[A>
2017-01-19 19:40:26 -0800

    Build schema in ORM.

--- database/models.js
+++ database/models.js
@@ -1,19 +1,68 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('scrubd', 'root', '');
 
-const Users = db.define('User', {
-  username: Sequelize.CHAR(20)
+const User = db.define('User', {
+  name: {
+    type: Sequelize.CHAR(20),
+    unique: true
+  }
+}, {
+  timestamps: false
 });
 
-const Videos = db.define('User', {
-  url: Sequelize.CHAR(255),
+const Video = db.define('Video', {
+  url: {
+    type: Sequelize.CHAR(255),
+    unique: true
+  },
   views: {
     type: Sequelize.INTEGER,
     defaultValue: 0
   }
+}, {
+  timestamps: false
 });
 
-const Comments = db.define('User', {
+const Comment = db.define('Comment', {
   comment: Sequelize.TEXT,
   time_stamp: Sequelize.DECIMAL(10, 3)
+}, {
+  timestamps: false
 });
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+User.hasMany(Video);
+Video.belongsTo(User);
+
+Video.hasMany(Comment);
+Comment.belongsTo(Video);
+
+User.sync();
+Video.sync();
+Comment.sync();
+
+// User.find()
+// .then( (result) => {
+//   if (result) {
+//     console.log(result.dataValues);
+//   }
+// });
+
+// var hey;
+
+// User.create({name: 'MARC'})
+// .then(instance => {
+//   if (instance) {
+//     hey = instance;
+//     console.log(instance);
+//   }
+// });
+
+// Video.create({url: 'http://docs.sequelizejs.com/en/latest/docs/'})
+// .then(instance => {
+//   if (instance) {
+//     instance.setUser(hey);
+//   }
+// });
