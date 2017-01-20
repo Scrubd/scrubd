3681f1674250a27af2ad2fedaf39ae006ef74dc9
Joseph <joseph.sc.luk@gmail.com>
2017-01-20 13:38:09 -0800

    added server file

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var morgan = require('morgan');
+
+app.listen(3000)
+
+// app.use(express.static('dist'));
+app.use(express.static('../dist'))
+
+// app.get('/', function(req, res) {
+//     // res.send("heyy");
+//     res.sendFile(path.join(__dirname + '/../dist/index.html'));
+
+//     console.log("heyyy");
+// });
