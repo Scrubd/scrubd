57a79d142c67f0ca131377307917706d31a3297e
Joseph <joseph.sc.luk@gmail.com>
2017-01-23 13:19:37 -0800

    create routes for getting and posting of comments

--- server/server.js
+++ server/server.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var morgan = require('morgan');
+const express = require('express');
+const router = require('express').Router();
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
 const controller = require('../database/controller');
+const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser')
 
-app.listen(8080)
+app.listen(port)
 
+//Static serve files
 app.use(express.static('../dist'))
 
+// in case we want json encoded bodies
+app.use(bodyParser.json());
+
 //to retrieve ALL comments. 
-app.get('/api/comment', function(req, res){
-  controller.getComments;
-})
+app.get('/api/comments', controller.comments.get)
 
 //to post a comment
-app.post('/api/comment', function(req, res){
-  controller.sendComment;
-})
+app.post('/api/comments', controller.comments.post)
+  //get comment from req.body
+    //use comment data as argument for sendComment controller
+      //invoke controller
+
 
 
 
