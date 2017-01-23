5813c96854de899e49b3da6f68eff070eb3fa18c
Joseph <joseph.sc.luk@gmail.com>
2017-01-23 12:18:50 -0800

    add express listeners

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var morgan = require('morgan');
+const controller = require('../database/controller');
 
 app.listen(8080)
 
 app.use(express.static('../dist'))
 
-//trivial server.js comment
+//to retrieve ALL comments. 
+app.get('/api/comment', function(req, res){
+  controller.getComments;
+})
+
+//to post a comment
+app.post('/api/comment', function(req, res){
+  controller.sendComment;
+})
+
+
+
+
