1925e83de68216bec5ea586f1abc10eb83060ca2
MarcManiez <marc.maniez@gmail.com[A>
2017-01-23 15:22:12 -0800

    Retrieve comments for individual video.

--- database/controller.js
+++ database/controller.js
@@ -9,13 +9,44 @@ const Comment = models.Comment;
 module.exports = {
   comments: {
     get: function (req, res) {
-      Comment.find();
+      const url = req.url.URL;
+      Video.findOne({where: {url: url} })
+        .then(video => {
+          Comment.findAll({
+            where: {VideoId: video.dataValues.id},
+            order: 'time_stamp ASC',
+            attributes: ['comment', 'time_stamp'],
+            include: [
+              {
+                model: User,
+                attributes: ['name']
+              }
+            ]
+          })
+            .then(results => {
+              if (results) {
+                const comments = [];
+                for (let item of results) {
+                  comments.push(item.dataValues);
+                }
+                console.log(comments);
+              }
+            });
+        });
     },
     post: function (req, res) {
-      const {comment, time_stamp} = req.body;
+      const {comment, time_stamp, url, name} = req.body;
       Comment.create({comment: comment, time_stamp: time_stamp})
-        .then(instance => {
-          res.sendStatus(201);
+        .then(comment => {
+          Video.findOne({where: {url: url} })
+            .then(video => {
+              comment.setVideo(video);
+              User.findOne({where: {name: name} })
+                .then(user => {
+                  comment.setUser(user);
+                  res.sendStatus(201);
+                });
+            });
         })
         .catch(err => {
           res.status(400).end(JSON.stringify(err));
