d1cc1b743e6d941eb18220b29695c22e1bb37ecd
MarcManiez <marc.maniez@gmail.com[A>
2017-01-23 12:36:28 -0800

    Create comments.post method.

--- database/controller.js
+++ database/controller.js
@@ -6,4 +6,20 @@ const User = models.User;
 const Video = models.Video;
 const Comment = models.Comment;
 
-console.log(models);
+module.exports = {
+  comments: {
+    get: function (req, res) {
+      Comment.find();
+    },
+    post: function (req, res) {
+      const {comment, time_stamp} = req.body;
+      Comment.create({comment: comment, time_stamp: time_stamp})
+        .then(instance => {
+          res.sendStatus(201);
+        })
+        .catch(err => {
+          res.status(400).end(JSON.stringify(err));
+        });
+    }
+  }
+};
