f4c2c8a024f17778bdc4dbe40c02f0470a236916
Joseph <joseph.sc.luk@gmail.com>
2017-01-23 11:36:39 -0800

    Cleaned up server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,5 @@ var morgan = require('morgan');
 
 app.listen(8080)
 
-// app.use(express.static('dist'));
 app.use(express.static('../dist'))
 
-// app.get('/', function(req, res) {
-//     // res.send("heyy");
-//     res.sendFile(path.join(__dirname + '/../dist/index.html'));
-
-//     console.log("heyyy");
-// });
