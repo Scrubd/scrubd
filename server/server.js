fae7e6f0605de6b1306d1bb05d59aae58e3c865b
Joseph <joseph.sc.luk@gmail.com>
2017-01-23 11:21:43 -0800

    assessment practice

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var morgan = require('morgan');
 
-app.listen(3000)
+app.listen(8080)
 
 // app.use(express.static('dist'));
 app.use(express.static('../dist'))
