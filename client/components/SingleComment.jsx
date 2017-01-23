b83d21ea89308fbff07b1e890ae9628392bd8cff
bartonyoung <barton.young1986@gmail.com>
2017-01-23 13:59:44 -0800

    Create function to target input value and video timeStamp

--- client/components/SingleComment.jsx
+++ client/components/SingleComment.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const SingleComment = () => (
-  <div className='container'>
-    <input placeholder="Submit comment..."></input>
-    <button>Submit</button>
-  </div>
-);
+class SingleComment extends React.Component {
+
+  constructor(props) {
+   super(props);
+  }
+
+  commentSubmit (event) {
+    var comment = this.refs.comment.value;
+    var video = document.getElementsByTagName('video');
+    var timeStamp = video.media.currentTime;
+    console.log('message:', comment, 'Current Time', timeStamp);
+  }
+
+  render() {
+    return (
+    <div className='container'>
+      <input ref="comment" placeholder="Submit comment..."/>
+      <button onClick={this.commentSubmit.bind(this)}>Submit</button>
+    </div>
+    );
+  }
+}
 
 export default SingleComment;
