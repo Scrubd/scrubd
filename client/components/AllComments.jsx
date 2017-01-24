3690abb6dfbf92622dd2e3e3b43a852c19276687
bartonyoung <barton.young1986@gmail.com>
2017-01-23 19:57:11 -0800

    Create a div for each comment returned from database.

--- client/components/AllComments.jsx
+++ client/components/AllComments.jsx
@@ -4,7 +4,7 @@ import Comment from './Comment.jsx';
 
 const AllComments = (props) => (
     <div>
-      {props.comments.map( (comment) => {
+      {props.comments.map( comment => {
         return <Comment comment={comment}/>
       })}
     </div>
