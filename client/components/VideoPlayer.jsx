18a10b25b5d442ef3f0ee4a952c4f0569db2765e
Joseph <joseph.sc.luk@gmail.com>
2017-01-24 13:34:42 -0800

    Alter video tag to include ID.

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoPlayer = (props) => (
   <div>
-    <video name="media" width="640" height="360" preload controls>
+    <video id="media" name="media" width="640" height="360" preload controls>
       <source src={props.currentVideo}/>
     </video>
   </div>
