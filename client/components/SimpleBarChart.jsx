5a4a3518a82e7e8359cfd0922586817aff672240
Joseph <joseph.sc.luk@gmail.com>
2017-01-24 15:39:29 -0800

    Increase number of bars in chart space.

--- client/components/SimpleBarChart.jsx
+++ client/components/SimpleBarChart.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // const {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 const data = [
+      {name: '02', uv: 4000},
+      {name: '04', uv: 3000},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', uv: 4000},
+      {name: 'Page B', uv: 3000},
+      {name: 'Page C', uv: 2000, pv: 0, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
       {name: 'Page A', uv: 4000},
       {name: 'Page B', uv: 3000},
       {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
@@ -10,12 +24,18 @@ const data = [
       {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
       {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
       {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+
 ];
 class SimpleBarChart extends React.Component {
   constructor (props) {
     super(props);
   }
-  // handleUvBarClick:
+  handleUvBarClick() {
+    var targetVid = document.getElementById("media");
+    targetVid.currentTime = 24;
+    console.log("clicked!");
+    console.log(d.data.key);
+  }
 
   render () {
     return (
@@ -27,7 +47,7 @@ class SimpleBarChart extends React.Component {
        <Tooltip/>
        <Legend />
        <Bar dataKey="pv" fill="#8884d8" onClick={this.handleUvBarClick} />
-       <Bar dataKey="uv" fill="#82ca9d" />
+       <Bar dataKey="suv" fill="#82ca9d" onClick={this.handleUvBarClick}/>
       </BarChart>
     );
   }
