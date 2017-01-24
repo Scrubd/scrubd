6a5ecba7fa50680eee8c06f8dae868fce96c6506
MarcManiez <marc.maniez@gmail.com[A>
2017-01-24 15:06:13 -0800

    Fix formatting on default store.

--- client/store.js
+++ client/store.js
@@ -5,7 +5,11 @@ import reducer from './reducers/index';
 
 const middleware = applyMiddleware(logger(), thunk);
 const defaultStore = {
-  video: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-  comments: [],
+  video: {
+    currentVideo: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+  },
+  comments: {
+    comments: [],
+  },
 };
 export default createStore(reducer, defaultStore, middleware);
