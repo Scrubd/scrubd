d519cf50ce8d28a483e44a4c58fa1ff4a2c0d994
Joseph <joseph.sc.luk@gmail.com>
2017-01-24 09:25:34 -0800

    Refactor axios helper function exports.

--- server/axios-helper.js
+++ server/axios-helper.js
@@ -1,19 +1,18 @@
 var axios = require('axios'); 
 
-var submitComment = function(data){
-  axios.post('/api/comments', data);
-};
+module.exports = {
+  submitComment: function (data) {
+    axios.post('/api/comments', data);
+  },
 
-var getComments = function(url){
-  console.log("get comments funcntion fired")
-  axios.get('/api/comments', {
-    params: {URL: url
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
+  getComments: function (url) {
+    console.log('get comments funcntion fired');
+    axios.get('/api/comments', {
+      params: {URL: url
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  }
 };
-
-module.exports.submitComment = submitComment;
-module.exports.getComments = getComments;
