9dfd482fd720ad1d054b3c2dec0e8c4f8430c64b
MarcManiez <marc.maniez@gmail.com[A>
2017-01-25 12:18:43 -0800

    Pseudo code for finding nearest time stamp.

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const VideoPlayer = (props) => (
-  <div>
-    <iframe src={props.currentVideo} width="640" height="360" frameBorder="0" allowFullScreen></iframe>
-  </div>
-);
+const VideoPlayer = (props) => {
+  const findNearestTimeStamp = (comments, timeStamp) => {
+    // Look for middle of array
+      // if timeStamp >= current comment < next comment
+        // return current comment's time stamp
+      // if timeStamp >= previous comment and < current comment
+        // return previous comment's time stamp
+      // if timeStamp < current comment's' time stamp
+        // recurse with (comments/2 1st half, timeStamp)
+      // if timeStamp > current comment's' time stamp
+        // recurse with (comments/2 2st half, timeStamp)
+  };
+  return (
+    <div>
+      <iframe src={props.currentVideo} width="640" height="360" frameBorder="0" allowFullScreen />
+    </div>
+  );
+};
 
 
 export default VideoPlayer;
