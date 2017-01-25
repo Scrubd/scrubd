6145959ef867c6c3ffa90765f3a43cdfa5338c91
MarcManiez <marc.maniez@gmail.com[A>
2017-01-25 13:51:40 -0800

    Write method to return closest timestamp.

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
+import Player from '@vimeo/player';
 
 class VideoPlayer extends React.Component {
 
   componentDidMount() {
     const findNearestTimeStamp = (comments, timeStamp) => {
-      // Look for middle of array
-        // if timeStamp >= current comment < next comment
-          // return current comment's time stamp
-        // if timeStamp >= previous comment and < current comment
-          // return previous comment's time stamp
-        // if timeStamp < current comment's' time stamp
-          // recurse with (comments/2 1st half, timeStamp)
-        // if timeStamp > current comment's' time stamp
-          // recurse with (comments/2 2st half, timeStamp)
+      const middleIndex = Math.floor(comments.length / 2);
+      const middle = comments[middleIndex];
+      const next = comments[middleIndex + 1] || middle;
+      const previous = comments[middleIndex - 1] || middle;
+      if (timeStamp === middle.time_stamp) {
+        return timeStamp;
+      } else if (timeStamp < comments[0].time_stamp) {
+        return comments[0].time_stamp;
+      } else if (timeStamp > comments[comments.length - 1].time_stamp) {
+        return comments[comments.length - 1].time_stamp;
+      } else if (timeStamp >= middle.time_stamp && timeStamp < next.time_stamp) {
+        return middle.time_stamp;
+      } else if (timeStamp >= previous.time_stamp && timeStamp < middle.time_stamp) {
+        return previous.time_stamp;
+      } else if (timeStamp < middle.time_stamp) {
+        return findNearestTimeStamp(comments.slice(0, middleIndex), timeStamp);
+      } else if (timeStamp > middle.time_stamp) {
+        return findNearestTimeStamp(comments.slice(middleIndex), timeStamp);
+      }
     };
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const that = this;
+    player.on('seeked', ((data) => {
+      const lol = findNearestTimeStamp(this.props.comments, data.seconds);
+      debugger;
+      // findNearestTimeStamp(this.props.comments);
+    }).bind(this));
   }
-  
+
   render() {
     return (
       <div>
