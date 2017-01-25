fb09e5a7655cc5621df611336dbe180b956ae631
MarcManiez <marc.maniez@gmail.com[A>
2017-01-25 12:27:13 -0800

    Refactor to class component.

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
-const VideoPlayer = (props) => {
-  const findNearestTimeStamp = (comments, timeStamp) => {
-    // Look for middle of array
-      // if timeStamp >= current comment < next comment
-        // return current comment's time stamp
-      // if timeStamp >= previous comment and < current comment
-        // return previous comment's time stamp
-      // if timeStamp < current comment's' time stamp
-        // recurse with (comments/2 1st half, timeStamp)
-      // if timeStamp > current comment's' time stamp
-        // recurse with (comments/2 2st half, timeStamp)
-  };
-  return (
-    <div>
-      <iframe src={props.currentVideo} width="640" height="360" frameBorder="0" allowFullScreen />
-    </div>
-  );
-};
+class VideoPlayer extends React.Component {
+
+  componentDidMount() {
+    const findNearestTimeStamp = (comments, timeStamp) => {
+      // Look for middle of array
+        // if timeStamp >= current comment < next comment
+          // return current comment's time stamp
+        // if timeStamp >= previous comment and < current comment
+          // return previous comment's time stamp
+        // if timeStamp < current comment's' time stamp
+          // recurse with (comments/2 1st half, timeStamp)
+        // if timeStamp > current comment's' time stamp
+          // recurse with (comments/2 2st half, timeStamp)
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <iframe src={this.props.currentVideo} width="640" height="360" frameBorder="0" allowFullScreen />
+      </div>
+    );
+  }
+}
 
 
 export default VideoPlayer;
