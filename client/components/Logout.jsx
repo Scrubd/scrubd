c8cba9d169774db0f60ef2ab1ad20092e94148a0
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 14:15:47 -0800

    Make Logout component into button.

--- client/components/Logout.jsx
+++ client/components/Logout.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function Logout({ sessionActive }) {
-
+export default function Logout({ dispatch }) {
   return (
-    <form action="#" onSubmit={() => {}}>
-      <div>
-        <label htmlFor="logout">Logout</label>
-        <input id="logout" placeholder="Username here..." />
-      </div>
-    </form>
+    <div>
+      <label htmlFor="logout">Logout</label>
+      <input type="button" id="logout" placeholder="Username here..." />
+    </div>
   );
 }
