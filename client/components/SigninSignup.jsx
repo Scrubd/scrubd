6c104e2347dab8ee7da0ac25db4735c4440effd1
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 15:08:26 -0800

    Refactor SigninSignup to use ref.

--- client/components/SigninSignup.jsx
+++ client/components/SigninSignup.jsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import { signupOrSignin } from '../actions/userActions';
 
 export default function SigninSignup({ dispatch }) {
+  let textInput = null;
+
   const submitHandler = (e) => {
-    dispatch(signupOrSignin({ name: e.currentTarget.firstChild.lastChild.value }));
+    dispatch(signupOrSignin({ name: textInput.value }));
   };
 
   return (
     <form action="#" onSubmit={submitHandler}>
       <div>
         <label htmlFor="signin-signup">Signin / Signup</label>
-        <input id="signin-signup" placeholder="Username here..." />
+        <input id="signin-signup" placeholder="Username here..." ref={(ref) => { textInput = ref; }} />
       </div>
     </form>
   );
