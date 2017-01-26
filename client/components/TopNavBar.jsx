c1f10a2baa582f359892a62d9f3d6eae97281242
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 09:33:37 -0800

    Map to state / dispatch. Toggle render on login.

--- client/components/TopNavBar.jsx
+++ client/components/TopNavBar.jsx
@@ -1,22 +1,15 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logout from './Logout.jsx';
 import SigninSignup from './SigninSignup.jsx';
 
-export default function TopNavBar({ sessionActive }) {
-  const consoling = () => {
-    console.log('don\'t cry');
-  };
+const TopNavBar = ({ sessionActive, dispatch }) => (
+  <nav>
+    { sessionActive ? (<Logout dispatch={dispatch} />) : (<SigninSignup dispatch={dispatch} />) }
+  </nav>
+);
 
-  return (
-    <nav>
-      <form action="#" onSubmit={() => { consoling(); }}>
-        <div>
-          <label htmlFor="login-logout">Login / Logout</label>
-          <input id="login-logout" placeholder="Username here..." />
-        </div>
-      </form>
-    </nav>
-  );
-}
-
+export default connect((state => ({
+  user: state.user,
+})))(TopNavBar);
