2469b80423e96fc58d535263803e8f9b9a8c044d
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 15:06:13 -0800

    Create logout button with click listener.

--- client/components/Logout.jsx
+++ client/components/Logout.jsx
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 export default function Logout({ dispatch }) {
   return (
     <div>
-      <label htmlFor="logout">Logout</label>
-      <input type="button" id="logout" placeholder="Username here..." />
+      <a href="" onClick={(e) => { e.preventDefault(); }}>Logout</a>
     </div>
   );
 }
