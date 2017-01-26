85a28b469a50807974dc2c57ef16efb1e66b03d4
MarcManiez <marc.maniez@gmail.com[A>
2017-01-25 23:15:06 -0800

    Add redux files for login/logout functionality.

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import comments from './commentsReducers';
 import video from './videoReducers';
+import user from './userReducers';
 
 export default combineReducers({
   comments,
   video,
+  user,
 });
