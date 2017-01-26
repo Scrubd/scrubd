636792292cd5b703944c4657343b670ba5498a8a
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 10:42:19 -0800

    Add reducers for sign up and sign in attempts.

--- client/reducers/userReducers.js
+++ client/reducers/userReducers.js
@@ -1,4 +1,14 @@
 export default function reducer(state, action) {
   const newState = Object.assign({}, state);
+  switch (action.type) {
+  case 'SIGNUP_FULFILLED':
+  case 'SIGNIN_FULFILLED':
+    state.sessionActive = true;
+    state.name = action.payload.name;
+    break;
+  case 'SIGNIN_OR_SIGNUP_FAILED':
+    console.log(action.payload.response);
+  // no default
+  }
   return newState;
 }
