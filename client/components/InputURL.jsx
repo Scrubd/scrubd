37a4311ea9a6cf0221c92edc15f04d7d5c21c410
bartonyoung <barton.young1986@gmail.com>
2017-01-26 11:57:57 -0800

    Check for valid Vimeo URLS.

--- client/components/InputURL.jsx
+++ client/components/InputURL.jsx
@@ -1,17 +1,33 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const validUrl = require('valid-url');
 // import { fetchComments } from '../actions/commentsActions';
 // import VideoPlayer from './VideoPlayer.jsx';
 // import CommentBox from './CommentBox.jsx';
 
 class InputURL extends React.Component {
 
-  render() {
+  submitURL () {
+    event.preventDefault();
+    let url = this.refs.url.value;
+    this.refs.url.value = '';
+     if (validUrl.isUri(url) && url.includes('vimeo')) {
+        let final = url.substr(url.lastIndexOf('/') + 1);
+        url = `https://player.vimeo.com/video/${final}`;
+        console.log(url);
+     } else {
+        alert('PLEASE ENTER A VALID VIMEO URL');
+    }
+
+  }
+
+  render () {
     return (
       <div className="container">
-        <input ref="comment" placeholder="Submit comment..." />
-        <button>Submit</button>
+        <input ref="url" placeholder="Add a video..." />
+        <button onClick={this.submitURL.bind(this)}>Submit</button>
       </div>
     );
   }
