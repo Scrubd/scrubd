4b37ccea6410ccc705b94f3ee8a723bf3c35af4e
Joseph <joseph.sc.luk@gmail.com>
2017-01-26 15:48:31 -0800

    Add thumbsdown submit feature.

--- client/components/SingleComment.jsx
+++ client/components/SingleComment.jsx
@@ -34,14 +34,57 @@ class SingleComment extends React.Component {
       }).bind(this));
   }
 
+  thumbsUpSubmit () {
+    event.preventDefault();
+    var iframe = document.querySelector('iframe');
+    var player = new Player(iframe);
+    var videoSource = player.element.getAttribute('src');
+    var timeStamp;
+    var data;
+    // var that = this;
+
+    player.getCurrentTime()
+      .then(((seconds) => {
+        data = {
+          name: 'JOSEPH',
+          comment: ":thumbsup:",
+          URL: videoSource,
+          time_stamp: seconds
+        };
+        this.props.dispatch(submitComment(data));
+      }).bind(this));
+  }
+
+  thumbsDownSubmit () {
+    event.preventDefault();
+    var iframe = document.querySelector('iframe');
+    var player = new Player(iframe);
+    var videoSource = player.element.getAttribute('src');
+    var timeStamp;
+    var data;
+    // var that = this;
+
+    player.getCurrentTime()
+      .then(((seconds) => {
+        data = {
+          name: 'JOSEPH',
+          comment: ":thumbsdown:",
+          URL: videoSource,
+          time_stamp: seconds
+        };
+        this.props.dispatch(submitComment(data));
+      }).bind(this));
+  }
+
+
 
   render() {
     return (
     <div className='container'>
       <input ref="comment" placeholder="Submit comment..."/>
       <button onClick={this.commentSubmit.bind(this)}>Submit</button>
-      <span> {emojify(':thumbsup:', {output: 'unicode'})} </span>
-      <span> {emojify(':thumbsdown:', {output: 'unicode'})} </span>      
+      <span onClick={this.thumbsUpSubmit.bind(this)}> {emojify(':thumbsup:', {output: 'unicode'})} </span>
+      <span onClick={this.thumbsDownSubmit.bind(this)}> {emojify(':thumbsdown:', {output: 'unicode'})} </span>      
     </div>
     );
   }
