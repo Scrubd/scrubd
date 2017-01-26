063a6e68c6d8c32874a81ebbca058c0218e0436b
MarcManiez <marc.maniez@gmail.com[A>
2017-01-25 23:42:59 -0800

    Add basic form and test function on submit.

--- client/components/TopNavBar.jsx
+++ client/components/TopNavBar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TopNavBar = () => (
-  <nav>
-  </nav>
+export default function TopNavBar() {
+  const consoling = () => {
+    console.log('don\'t cry');
+  };
+
+  return (
+    <nav>
+      <form action="#" onSubmit={() => { consoling(); }}>
+        <div>
+          <label htmlFor="login-logout">Login / Logout</label>
+          <input id="login-logout" placeholder="Username here..." />
+        </div>
+      </form>
+    </nav>
   );
+}
 
-
-export default TopNavBar;
