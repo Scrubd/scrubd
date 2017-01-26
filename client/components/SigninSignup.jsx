63bb2a14a18e681f7fd466e36a944a1c4b8b4da9
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 10:17:13 -0800

    Send sign in or sign up action on submit.

--- client/components/SigninSignup.jsx
+++ client/components/SigninSignup.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signupOrSignin } from '../actions/userActions';
 
-export default function SigninSignup({ sessionActive }) {
+export default function SigninSignup({ dispatch }) {
+  const submitHandler = (e) => {
+    dispatch(signupOrSignin({ name: e.currentTarget.firstChild.lastChild.value }));
+  };
 
   return (
-    <form action="#" onSubmit={() => {}}>
+    <form action="#" onSubmit={submitHandler}>
       <div>
         <label htmlFor="signin-signup">Signin / Signup</label>
         <input id="signin-signup" placeholder="Username here..." />
