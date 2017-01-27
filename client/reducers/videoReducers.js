cb90f4e818a0a000eb1b543f86d826b87ba282f1
bartonyoung <barton.young1986@gmail.com>
2017-01-26 18:24:34 -0800

    Create videoReducer.

--- client/reducers/videoReducers.js
+++ client/reducers/videoReducers.js
@@ -1,4 +1,19 @@
-export default function reducer(state, action) {
+import store from '../store';
+
+export default function reducer (state, action) {
   const newState = Object.assign({}, state);
-  return newState;
+  switch (action.type) {
+    case 'SUBMIT_VIDEO_FULFILLED': {
+     // newState.currentVideo = action.payload;
+     console.log('video successfully submitted');
+      break;
+    }
+    case 'SUBMIT_VIDEO_FAILED': {
+    console.log('Comments fetch failed.');
+    break;
+  }
+  // no default
+}
+
+return newState;
 }
