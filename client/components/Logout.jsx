72e270c19b50e429d6da1afad985ddf4a8ce6b7b
MarcManiez <marc.maniez@gmail.com[A>
2017-01-26 16:46:15 -0800

    Dispatch logout request on click.

--- client/components/Logout.jsx
+++ client/components/Logout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signout } from '../actions/userActions';
 
 export default function Logout({ dispatch }) {
   return (
     <div>
-      <a href="" onClick={(e) => { e.preventDefault(); }}>Logout</a>
+      <a href="" onClick={(e) => { e.preventDefault(); dispatch(signout); }}>Logout</a>
     </div>
   );
 }
