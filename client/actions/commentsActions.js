34c8623c2ecef2820023fb44156c1b37922ee63e
bartonyoung <barton.young1986@gmail.com>
2017-01-26 18:53:21 -0800

    Add/remove console logs

--- client/actions/commentsActions.js
+++ client/actions/commentsActions.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
-export function fetchComments(url) {
-  return (dispatch) => {
+export function fetchComments (url) {
+  return dispatch => {
     axios.get('/api/comments', {
-      params: { URL: url },
+      params: { URL: url }
     })
-    .then((response) => {
+    .then(response => {
       dispatch({ type: 'FETCH_COMMENTS_FULFILLED', payload: response.data });
     })
-    .catch((err) => {
+    .catch(err => {
       dispatch({ type: 'FETCH_COMMENTS_FAILED', payload: err });
     });
   };
 }
 
-export function submitComment(data) {
-  return (dispatch) => {
+export function submitComment (data) {
+return dispatch => {
     axios.post('/api/comments', data)
-    .then((response) => {
+    .then(response => {
       dispatch({ type: 'SUBMIT_COMMENT_FULFILLED', payload: JSON.parse(response.config.data) });
     })
-    .catch((err) => {
+    .catch(err => {
       dispatch({ type: 'SUBMIT_COMMENT_FAILED', payload: err });
     });
   };
