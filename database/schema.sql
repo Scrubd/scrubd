f3fef9ce2cf9f17e631eae4a7e69a957b535ea47
MarcManiez <marc.maniez@gmail.com[A>
2017-01-27 15:09:44 -0800

    Set schema to create db if it does not exist.

--- database/schema.sql
+++ database/schema.sql
@@ -1,33 +1,33 @@
 -- run with mysql -u root < path/to/schema.sql
 
-CREATE DATABASE scrubd;
+CREATE DATABASE IF NOT EXISTS scrubd;
 
 USE scrubd;
 
 -- DROP TABLE IF EXISTS Users;
 
-CREATE TABLE Users (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  name CHAR(20) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE Users (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   name CHAR(20) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE Videos (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  url CHAR(255) NOT NULL,
-  views INTEGER DEFAULT 0,
-  -- user_id integer NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE Videos (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   url CHAR(255) NOT NULL,
+--   views INTEGER DEFAULT 0,
+--   -- user_id integer NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE Comments (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  comment text NOT NULL,
-  -- user_id integer NOT NULL,
-  -- video_id integer NOT NULL,
-  time_stamp DECIMAL(10, 3) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE Comments (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   comment text NOT NULL,
+--   -- user_id integer NOT NULL,
+--   -- video_id integer NOT NULL,
+--   time_stamp DECIMAL(10, 3) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
 -- ALTER TABLE Videos ADD FOREIGN KEY (user_id) REFERENCES Users (id);
 -- ALTER TABLE Comments ADD FOREIGN KEY (user_id) REFERENCES Users (id);
