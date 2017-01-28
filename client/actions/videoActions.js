a1fb4158bd783b6b08a6056662ed0cbdee5c359a
MarcManiez <marc.maniez@gmail.com[A>
2017-01-27 22:25:10 -0800

    Return the promise in order to chain it.

--- client/actions/videoActions.js
+++ client/actions/videoActions.js
@@ -3,9 +3,10 @@ import { fetchComments } from '../actions/commentsActions';
 
 export function submitURL(data) {
   return (dispatch) => {
-    axios.post('/api/videos', data)
+    return axios.post('/api/videos', data)
     .then((response) => {
       dispatch({ type: 'SUBMIT_VIDEO_FULFILLED', payload: response.data });
+      return response.data;
     })
     .catch((err) => {
       dispatch({ type: 'SUBMIT_VIDEO_FAILED', payload: err });
