911cf5c13faaf583b79bb26015d6321964c3157c
Joseph <joseph.sc.luk@gmail.com>
2017-01-27 16:31:58 -0800

    Delete comments.

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -14,17 +14,6 @@ class VideoPlayer extends React.Component {
 
     player.on('seeked', ((data) => {
       const anchor = findNearestTimeStamp(this.props.comments, data.seconds);
-      // scroller.scrollTo(anchor.toString(), {
-      //   duration: 300,
-      //   smooth: true,
-      // });
-      // console.log(anchor)
-      // var topPos = document.getElementById(anchor.toString()).offsetTop;
-      // console.log("topPos: " + topPos);
-      // console.log("container scrolltop: " + document.getElementById('commentsContainer').scrollTop);
-      // document.getElementById('commentsContainer').scrollTop = topPos;
-      // var commentsContainer = document.getElementById('commentsContainer')
-      // commentsContainer.scrollTo(anchor);     
       var element = document.getElementById(anchor.toString())
       element.scrollIntoView();
 
