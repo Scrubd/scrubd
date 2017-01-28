57572bc14fae504d59e516000c9c721da1fdda02
MarcManiez <marc.maniez@gmail.com[A>
2017-01-27 23:58:19 -0800

    Fix comments to display on newly added videos.

--- client/reducers/commentsReducers.js
+++ client/reducers/commentsReducers.js
@@ -3,36 +3,35 @@ import store from '../store';
 export default function reducer(state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
-    case 'FETCH_COMMENTS_FULFILLED': {
-      newState.comments = action.payload;
-      break;
-    }
-    case 'FETCH_COMMENTS_FAILED': {
-      console.log('Comments fetch failed.');
-      break;
-    }
-    case 'SUBMIT_COMMENT_FULFILLED': {
-      const { comment, name, time_stamp } = action.payload;
-      const newComment = {
-        User: { name },
-        comment,
-        time_stamp,
-      };
-      newState.comments = newState.comments.slice();
-      for (let i = 0; i < newState.comments.length; i += 1) {
-        if (newState.comments[i].time_stamp > newComment.time_stamp) {
-          newState.comments.splice(i, 0, newComment);
-          break;
-        }
+  case 'FETCH_COMMENTS_FULFILLED': {
+    newState.comments = action.payload;
+    break;
+  }
+  case 'FETCH_COMMENTS_FAILED': {
+    console.log('Comments fetch failed.');
+    break;
+  }
+  case 'SUBMIT_COMMENT_FULFILLED': {
+    const { comment, name, time_stamp } = action.payload;
+    const newComment = {
+      User: { name },
+      comment,
+      time_stamp,
+    };
+    newState.comments = newState.comments.slice();
+    for (let i = 0; i < newState.comments.length; i += 1) {
+      if (newState.comments[i].time_stamp > newComment.time_stamp) {
+        newState.comments.splice(i, 0, newComment);
+        break;
       }
-      console.log(newState.comments);
-      console.log('Comment successfully registered.');
-      break;
     }
-    case 'SUBMIT_COMMENT_FAILED': {
-      console.log('Comment failed to register.');
-      break;
-    }
+    newState.comments.push(newComment);
+    break;
+  }
+  case 'SUBMIT_COMMENT_FAILED': {
+    console.log('Comment failed to register.');
+    break;
+  }
   // no default
   }
   return newState;
