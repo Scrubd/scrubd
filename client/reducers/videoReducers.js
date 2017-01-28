43653f4f3f25c815f542c4d71d42918d3bbf5b52
MarcManiez <marc.maniez@gmail.com[A>
2017-01-28 10:48:20 -0800

    Add fetched videos to state or send error message.

--- client/reducers/videoReducers.js
+++ client/reducers/videoReducers.js
@@ -12,6 +12,14 @@ export default function reducer(state, action) {
       console.log('Submit video failed.');
       break;
     }
+    case 'FETCH_VIDEOS_FULFILLED': {
+      newState.videos = action.payload;
+      break;
+    }
+    case 'FETCH_VIDEOS_FAILED': {
+      console.log('Could not fetch videos');
+      break;
+    }
   // no default
   }
 
