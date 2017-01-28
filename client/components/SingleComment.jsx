eeea25a0c3d6f7b2d3981df35ed1bfe63761adc2
MarcManiez <marc.maniez@gmail.com[A>
2017-01-27 23:21:46 -0800

    Submit comment with current user's name.

--- client/components/SingleComment.jsx
+++ client/components/SingleComment.jsx
@@ -8,33 +8,32 @@ class SingleComment extends React.Component {
 
   commentSubmit() {
     event.preventDefault();
-    let comment = this.refs.comment.value;
+    const comment = this.refs.comment.value;
     this.refs.comment.value = '';
-    let iframe = document.querySelector('iframe');
-    let player = new Player(iframe);
-    let videoSource = player.element.getAttribute('src');
-    let timeStamp;
-    let data;
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const videoSource = player.element.getAttribute('src');
 
     player.getCurrentTime()
       .then((seconds) => {
-        data = {
-          name: 'JOSEPH',
+        return {
+          name: this.props.name,
           comment,
           URL: videoSource,
           time_stamp: seconds,
         };
+      })
+      .then((data) => {
         this.props.dispatch(submitComment(data));
       });
   }
 
-
   render() {
     return (
       <div className="container">
-      <input ref="comment" placeholder="Submit comment..."  />
-      <button onClick={this.commentSubmit.bind(this)}>Submit</button>
-    </div>
+        <input ref="comment" placeholder="Submit comment..." />
+        <button onClick={this.commentSubmit.bind(this)}>Submit</button>
+      </div>
     );
   }
 }
