07d551798522ba82098088f1ed1b6c78bf412785
MarcManiez <marc.maniez@gmail.com[A>
2017-01-28 11:47:08 -0800

    Elliminate redundant return statement.

--- client/components/AllComments.jsx
+++ client/components/AllComments.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Comment from './Comment.jsx';
 
-const AllComments = (props) => (
-    <div>
-      {props.comments.map( (comment, index) => {
-        return <Comment key={index} comment={comment}/>
-      })}
-    </div>
-  );
+const AllComments = props => (
+  <div>
+    {props.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
+  </div>
+);
 
 export default AllComments;
 
