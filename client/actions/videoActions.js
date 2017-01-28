4bacf0c9ad21f7447af3c4222ca0cc920d3d8a4a
MarcManiez <marc.maniez@gmail.com[A>
2017-01-28 10:47:51 -0800

    Add action to get videos.

--- client/actions/videoActions.js
+++ client/actions/videoActions.js
@@ -4,12 +4,24 @@ import { fetchComments } from '../actions/commentsActions';
 export function submitURL(data) {
   return (dispatch) => {
     return axios.post('/api/videos', data)
-    .then((response) => {
-      dispatch({ type: 'SUBMIT_VIDEO_FULFILLED', payload: response.data });
-      return response.data;
-    })
-    .catch((err) => {
-      dispatch({ type: 'SUBMIT_VIDEO_FAILED', payload: err });
-    });
+      .then((response) => {
+        dispatch({ type: 'SUBMIT_VIDEO_FULFILLED', payload: response.data });
+        return response.data;
+      })
+      .catch((err) => {
+        dispatch({ type: 'SUBMIT_VIDEO_FAILED', payload: err });
+      });
+  };
+}
+
+export function getVideos() {
+  return (dispatch) => {
+    return axios.get('/api/videos')
+      .then((response) => {
+        dispatch({ type: 'FETCH_VIDEOS_FULFILLED', payload: response.data });
+      })
+      .catch((err) => {
+        dispatch({ type: 'FETCH_VIDEOS_FAILED', payload: err });
+      });
   };
 }
