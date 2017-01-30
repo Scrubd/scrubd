a8d6712d33fb74444f1c4beb4c880a5017c8b58e
MarcManiez <marc.maniez@gmail.com[A>
2017-01-30 11:29:39 -0800

    Add videos to browsing list upon input.

--- client/components/InputURL.jsx
+++ client/components/InputURL.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import { submitURL } from '../actions/videoActions';
+import { submitURL, fetchVideos } from '../actions/videoActions';
 import { fetchComments } from '../actions/commentsActions';
 import { loadVideo } from '../componentHelpers';
 
@@ -20,6 +20,7 @@ class InputURL extends React.Component {
         .then((video) => {
           window.localStorage.setItem('currentVideo', JSON.stringify(data));
           this.props.dispatch(fetchComments(data.url));
+          this.props.dispatch(fetchVideos());
         });
       loadVideo(url);
     } else {
