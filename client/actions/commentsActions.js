ee560da7d8e6a174fc03e1bc79e18cb7b586fcd7
MarcManiez <marc.maniez@gmail.com[A>
2017-01-30 13:59:11 -0800

    Refactor into simple arrow functions.

--- client/actions/commentsActions.js
+++ client/actions/commentsActions.js
@@ -1,27 +1,21 @@
 import axios from 'axios';
 
 export function fetchComments(url) {
-  return (dispatch) => {
-    return axios.get('/api/comments', {
-      params: { URL: url },
-    })
+  return dispatch => axios.get('/api/comments', { params: { URL: url } })
     .then((response) => {
       dispatch({ type: 'FETCH_COMMENTS_FULFILLED', payload: response.data });
     })
     .catch((err) => {
       dispatch({ type: 'FETCH_COMMENTS_FAILED', payload: err });
     });
-  };
 }
 
 export function submitComment(data) {
-  return (dispatch) => {
-    return axios.post('/api/comments', data)
+  return dispatch => axios.post('/api/comments', data)
     .then((response) => {
       dispatch({ type: 'SUBMIT_COMMENT_FULFILLED', payload: JSON.parse(response.config.data) });
     })
     .catch((err) => {
       dispatch({ type: 'SUBMIT_COMMENT_FAILED', payload: err });
     });
-  };
 }
